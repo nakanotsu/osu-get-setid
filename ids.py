810c8a02dfcbd5d61ceb6c2f2af3d3a6e5b62dc4
pohmx <34806678+pohmx@users.noreply.github.com>
2021-03-29 10:37:55 -0300

    first commit

---
+++ ids.py
@@ -0,0 +1,11 @@
+#Pohmx development 2021
+#Free of use! Python 3.9x
+#You will need -pip install pyperclip- in order to use it.
+
+import os, re, pyperclip
+
+beatmaps = "\n".join(os.listdir('G:/osu!/Songs'))
+pattern = '^[0-9]*\s'
+processed = [n.strip(' ') for n in re.findall(pattern, beatmaps, re.MULTILINE)]
+clipboard = ",".join(processed)
+pyperclip.copy(clipboard)
