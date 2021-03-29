fadf397dd36f98795e630ebf3e76cf9aca148a0b
pohmx <34806678+pohmx@users.noreply.github.com>
2021-03-29 11:03:13 -0300

    empty

--- ids.py
+++ ids.py
@@ -1,5 +1,4 @@
 #Pohmx development 2021
-
 import os, re, pyperclip
 
 beatmaps = "\n".join(os.listdir('G:/osu!/Songs'))
@@ -7,4 +6,5 @@ pattern = '^[0-9]*\s'
 processed = [n.strip(' ') for n in re.findall(pattern, beatmaps, re.MULTILINE)]
 clipboard = ",".join(processed)
 pyperclip.copy(clipboard)
-print('copied')
+
+print('copied to clipboard!')
